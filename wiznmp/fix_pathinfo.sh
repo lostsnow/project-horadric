9949ab18a785d7d14e261383093dd14b8cdc9547
lostsnow@gmail.com <lostsnow@gmail.com@da9ce52a-2b44-11df-be6c-f52728ce0ce6>
2010-05-21 06:27:39 +0000

    add nginx/php pathinfo securit fix script

--- wiznmp/fix_pathinfo.sh
+++ wiznmp/fix_pathinfo.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-php_dir=/usr/local/php
+php_dir=$1
 
-echo "Please input your php path:"
-read -p "(Default path: /usr/local/php):" php_dir
-if [ "$php_dir" = "" ]; then
-    php_dir=/usr/local/php
+if [ ! $php_dir ] || [ ! -d $php_dir ]; then
+    echo "Please input your php path:"
+    read -p "(Default path: /usr/local/php):" php_dir
+    if [ ! $php_dir ]; then
+        php_dir=/usr/local/php
+    else
+        if [ ! -d $php_dir ]; then
+            echo "invalid directory."
+            exit 0
+        fi
+    fi
 fi
 
+echo ""
+echo "current php.ini"
+cat $php_dir/etc/php.ini |grep fix_pathinfo
+echo ""
+echo "modify php.ini"
 sed -i 's/; cgi.fix_pathinfo=0/cgi.fix_pathinfo=0/g' $php_dir/etc/php.ini
+cat $php_dir/etc/php.ini |grep fix_pathinfo
+echo ""
+echo "reload php.ini"
 $php_dir/sbin/php-fpm reload
